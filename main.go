c674f24ac1bab046df33d6cdfbe6bdd2415bcb63
Oreeeee <oree3ee@gmail.com>
2024-12-15 23:57:36 +0100

    Successful IRC connection!

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func HandleIRC(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Handling IRC connection")
+	var sentNames bool
+	for {
+		recvBuf := make([]byte, 1024)
+		n, err := conn.Read(recvBuf)
+		if err != nil {
+			fmt.Println(err)
+		}
+		msg := strings.Replace(string(recvBuf[:n]), "\n", "", -1)
+		fmt.Printf("Read %d bytes, message: %s\n", n, msg)
+		if strings.Contains(msg, "NAMES #peppy-osu-r") {
+			sentNames = true
+		}
+		if sentNames {
+			for _, str := range []string{"#osu", "#global"} {
+				fmt.Println("Jfjfsdjfjsfjsj fjdsfj,diohjkhjla " + str)
+				conn.Write([]byte(":peppy-osu!OSU@localhost JOIN :" + str + "\n"))
+			}
+
+		}
+	}
+}
 
 func main() {
 	fmt.Println("Hello World")
+
+	l, err := net.Listen("tcp", "127.0.0.1:6667")
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+
+	fmt.Println("Listening on 127.0.0.1:6667")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go HandleIRC(conn)
+	}
 }
