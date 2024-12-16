5c44c25c105a492e0e1ccae92baf5dde68c204f3
Oreeeee <oree3ee@gmail.com>
2024-12-16 20:34:44 +0100

    Separate recv and send threads

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"capstan/irc"
 	"capstan/structs"
 	"fmt"
 	"net"
@@ -9,35 +8,9 @@ import (
 )
 
 func HandleIRC(osuconn structs.OsuIrcClient) {
-	defer osuconn.Conn.Close()
 	fmt.Println("Handling IRC connection")
-	for {
-		recvBuf := make([]byte, 1024)
-		n, err := osuconn.Conn.Read(recvBuf)
-		if err != nil {
-			fmt.Println(err)
-		}
-		msg := strings.Replace(string(recvBuf[:n]), "\r", "", -1)
-		msg = strings.Replace(msg, "\n", "", -1)
-		msgSplit := strings.Split(msg, " ")
-
-		command := msgSplit[0]
-		args := msgSplit[1:]
-		fmt.Printf("Read %d bytes, message: %s\n", n, msg)
-		switch command {
-		case irc.IRC_NICK:
-			nick := args[0]
-			fmt.Println("Nick received", nick)
-			osuconn.Nickname = nick
-			fmt.Printf("kutas %02x", osuconn.Nickname)
-		case irc.IRC_JOIN:
-			channelName := args[0]
-			fmt.Println("Join received from", osuconn.Nickname, "to channel", channelName)
-			osuconn.WriteJoinAck(channelName)
-		default:
-			fmt.Println("Unknown command", msgSplit[0])
-		}
-	}
+	go osuconn.SendLoop()
+	go osuconn.RecvLoop()
 }
 
 func main() {
--- structs/OsuIrcClient.go
+++ structs/OsuIrcClient.go
@@ -5,12 +5,47 @@ import (
 	"capstan/irc"
 	"fmt"
 	"net"
+	"strings"
 )
 
 type OsuIrcClient struct {
 	Conn     net.Conn
 	Nickname string
 	Hostname string
+}
+
+func (c *OsuIrcClient) RecvLoop() {
+	for {
+		recvBuf := make([]byte, 1024)
+		n, err := c.Conn.Read(recvBuf)
+		if err != nil {
+			fmt.Println(err)
+		}
+		msg := strings.Replace(string(recvBuf[:n]), "\r", "", -1)
+		msg = strings.Replace(msg, "\n", "", -1)
+		msgSplit := strings.Split(msg, " ")
+
+		command := msgSplit[0]
+		args := msgSplit[1:]
+		fmt.Printf("Read %d bytes, message: %s\n", n, msg)
+		switch command {
+		case irc.IRC_NICK:
+			nick := args[0]
+			fmt.Println("Nick received", nick)
+			c.Nickname = nick
+			fmt.Printf("kutas %02x", c.Nickname)
+		case irc.IRC_JOIN:
+			channelName := args[0]
+			fmt.Println("Join received from", c.Nickname, "to channel", channelName)
+			//c.WriteJoinAck(channelName)
+		default:
+			fmt.Println("Unknown command", msgSplit[0])
+		}
+	}
+}
+
+func (c *OsuIrcClient) SendLoop() {
+	// TODO
 }
 
 func (c *OsuIrcClient) WriteJoinAck(channelName string) {
